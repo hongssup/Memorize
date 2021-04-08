6ef731cb7e41a77f5cda1b83ac31253506e1df11
seoyeon <hongssup0202@gmail.com>
2021-04-08 17:46:18 +0900

    Create custom shape Pie, using path

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -37,6 +37,8 @@ struct CardView: View {
             if card.isFaceUp {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                    .padding(5).opacity(0.4)
                 Text(card.content)
             } else {
                 if !card.isMatched {
@@ -52,12 +54,18 @@ struct CardView: View {
     private let cornerRadius: CGFloat = 10.0
     private let edgeLineWidth: CGFloat = 3
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    //다 뒤집혀 있으니깐 UI 수정을 위해 하나만 faceup으로 바꿔줌
     static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame() )
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(viewModel: game)
     }
+//    static var previews: some View {
+//        EmojiMemoryGameView(viewModel: EmojiMemoryGame() )
+//    }
 }
